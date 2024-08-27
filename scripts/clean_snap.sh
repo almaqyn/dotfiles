a2e531157b282b909cb1078be3e3e19ee5da2fcf
Darkhan Almaqyn <146783027+almaqyn@users.noreply.github.com>
2024-08-27 17:01:34 +0200

    Add scripts/clean_snap.sh

---
+++ scripts/clean_snap.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+set -eu
+LANG=en_US.UTF-8 snap list --all | awk '/disabled/{print $1, $3}' |
+    while read snapname revision; do
+        snap remove "$snapname" --revision="$revision"
+    done
+
